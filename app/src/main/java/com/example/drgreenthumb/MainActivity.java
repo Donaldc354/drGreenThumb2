53b77d51d47ca402dacf179ba9c4caf68a26f4a5
donaldc354 <donaldc354@gmail.com>
2019-06-14 14:25:58 -0400

    Merge branch 'master' of C:\Users\Donald\AndroidStudioProjects\Sender\drGreenThumb2 with conflicts.

--- app/src/main/java/com/example/drgreenthumb/MainActivity.java
+++ app/src/main/java/com/example/drgreenthumb/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.drgreenthumb;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
