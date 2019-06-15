d06fd2969f9d5b0c1f54af562f3eaa99a6a2e5dd
Kyle <kylepeters@mail.usf.edu>
2019-06-15 17:20:33 -0400

    added button click for settings

--- app/src/main/java/com/example/drgreenthumb/homePage.java
+++ app/src/main/java/com/example/drgreenthumb/homePage.java
@@ -1,7 +1,10 @@
 package com.example.drgreenthumb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class homePage extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class homePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        Button settingsButton = findViewById(R.id.btnSettings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homePage.this, settingsPage.class);
+                Bundle bundle = new Bundle();
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
+
     }
 }
