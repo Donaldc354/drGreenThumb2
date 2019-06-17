bc5eaead6f182b8a7fd10d0a06c71a8d02e2220b
donaldc354 <donaldc354@gmail.com>
2019-06-17 09:33:01 -0400

    Merge branch 'master' of C:\Users\Donald\AndroidStudioProjects\Sender\drGreenThumb2 with conflicts.

--- app/src/main/java/com/example/drgreenthumb/LoginActivity.java
+++ app/src/main/java/com/example/drgreenthumb/LoginActivity.java
@@ -1,31 +1,107 @@
-package com.example.drgreenthumb.ui.login;
+package com.example.drgreenthumb;
 
-import android.app.Activity;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.annotation.StringRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.drgreenthumb.R;
-import com.example.drgreenthumb.ui.login.LoginViewModel;
-import com.example.drgreenthumb.ui.login.LoginViewModelFactory;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
+    private EditText email;
+    private EditText password;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private Button button;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = (EditText)findViewById(R.id.txtEmail);
+        password = (EditText)findViewById(R.id.txtPassword);
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        button = (Button)findViewById(R.id.login);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == button){
+                    LoginUser();
+                }
+            }
+        });
+    }
+    public void LoginUser(){
+        String Email = email.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        mAuth.signInWithEmailAndPassword(Email, Password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            currentUser = mAuth.getCurrentUser();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(),
+                                    ProfileActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "couldn't login",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -125,3 +201,4 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
     }
 }
+*/
